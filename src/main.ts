c8d01b9ab378fa46a615a972df02b9098c8b7eb4
Sungwook Choi <sungwook.csw@gmail.com>
2023-07-15 22:08:49 +0900

    chore: enable ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      validationError: {
+        target: false,
+      },
+    }),
+  );
   await app.listen(3000);
+  const prismaService: PrismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 }
+
 bootstrap();
