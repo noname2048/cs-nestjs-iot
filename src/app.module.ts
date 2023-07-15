087f83c0fde887285ad3701250a2c62b08e2b96f
Sungwook Choi <sungwook.csw@gmail.com>
2023-07-15 22:34:27 +0900

    feat: append modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SensorsModule } from './sensors/sensors.module';
+import { DataModule } from './data/data.module';
 
 @Module({
-  imports: [],
+  imports: [SensorsModule, DataModule],
   controllers: [AppController],
   providers: [AppService],
 })
